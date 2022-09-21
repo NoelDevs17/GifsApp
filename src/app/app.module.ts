a25eeea5ae97338aca2974391e05aa8b597d964b
noeldrums17 <20175580@itla.edu.do>
2022-09-20 21:58:14 -0400

    -Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModuleModule } from './gifs/gifs-module.module';
 import { SharedModule } from './shared/shared.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { GifsPageComponent } from './gifs/gifs-page/gifs-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    GifsPageComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    GifsModuleModule
     
 
   ],
---
+++ src/app/gifs/gifs-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+
+
+
+@NgModule({
+  declarations: [],
+  
+  imports: [
+    CommonModule
+  ]
+
+})
+export class GifsModuleModule { }
